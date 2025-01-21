d44fcbcd1d8ff33b7467b87018a418a37130c680
Vandeputte Brice <boly38@gmail.com>
2025-01-21 13:21:43 +0100

    Fix #120 1day1bioclip exclude recent author

--- src/lib/ArrayUtil.js
+++ src/lib/ArrayUtil.js
@@ -10,3 +10,12 @@ export const arrayIsNotEmpty = arr => isSet(arr) && arr.length > 0;
 export const arrayGetRandomElement = array => {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+export const shuffledArray = array => array.sort(() => Math.random() - 0.5);
+export const shuffleArrayByFisherYates = array => {// Fisher-Yates algo
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Échange
+    }
+    return array;
+}
